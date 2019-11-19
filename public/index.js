bef9b77b51bb2beaa3c9d75701bf286e5ed731a6
ssvenlin <sandra.svenlin@gmail.com>
2019-11-19 13:31:35 +0200

    save

--- public/index.js
+++ public/index.js
@@ -1,30 +1,48 @@
-result = getProducts();
+$(function () {
+    let result = getProducts();
+    
+    let title="", text=""
+        for(let i = 0; i < result.length; i++){
+            id = result[i].id;
+            title = result[i].make;
+            text = result[i].model;
+            $("#product").append(product(id,title,text))
+            console.log(result[i].make);
+    }
+   })
+   var cart = []
+   function addToCart(id, amount) {
+       console.log("Adding " + id + " " + amount);
+       cart.push({id: id, amount: amount})
+       console.log(cart);
+       
+       if(productInCart()){
 
-let title="", text=""
-    for(var i = 0; i < result.length; i++){
-        title = result[i].make;
-        text = result[i].model;
-        $("#product").append(product(title,text))
-        console.log(result[i].make);
-        
+       }else{
+           console.log(cart);
+           
+       }
+   }
+
+   function productInCart() {
+       return false;
+   }
+
+function product(id, title, text, image="https://dummyimage.com/286x180/000/fff") {
+    let product = `[<div data="${id}" style="float:left; class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${image}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${title}</h5>
+      <p class="card-text">${text}</p>
+      <a onclick="addToCart(${id},1)" class="btn btn-primary">Lägg i varukorgen</a>
+    </div>
+  </div>] `
+return product;
 }
-
 function getProducts(){
-    let result = [{make: "Audi", model: "car"}]
+    let result = [{id: 1,make: "Audi", model: "A4"},{   id: 2, make: "Volvo", model: "car"}]
+    return result;
 }
 
-let product = `[<div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://dummyimage.com/286x180/000/fff" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>] `
 
-      $(function() {
-       $("#product").append (product);
-       $("#product").append (product);
-       $("#product").append (product);
-
-      })
+     
