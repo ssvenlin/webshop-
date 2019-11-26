07be55bf99d1872e0df4d853966d17e5a51123f2
ssvenlin <sandra.svenlin@gmail.com>
2019-11-26 10:12:16 +0200

    save

---
+++ public/server.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.use(express.static('public'))
